d3e2c26e150f8eaea3e07347e014d8b8e9836910
Debashish <debasishtewary5@gmail.com>
2023-04-04 02:36:24 +0530

    "Classes updated"

--- TypeScript/classes.js
+++ TypeScript/classes.js
@@ -1,8 +1,33 @@
 "use strict";
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var _Employee_id;
 class Employee {
+    get empId() {
+        return __classPrivateFieldGet(this, _Employee_id, "f");
+    }
+    set empId(id) {
+        __classPrivateFieldSet(this, _Employee_id, id, "f");
+    }
+    static getEmployeeCount() {
+        return 50;
+    }
     //constructor(){}
     constructor(id, name, address) {
-        this.id = id;
+        // id!:number;
+        // name!:string;
+        // address!:string; ! dont give errors
+        _Employee_id.set(this, void 0); //#-privvate
+        __classPrivateFieldSet(this, _Employee_id, id, "f");
         this.name = name;
         this.address = address;
     }
@@ -10,6 +35,16 @@ class Employee {
     getinformation() {
         //return this.name+" "+this.address;
         return `${this.name} stays at ${this.address}`;
+    }
+}
+_Employee_id = new WeakMap();
+console.log(Employee.getEmployeeCount());
+class manager extends Employee {
+    constructor(id, name, address) {
+        super(id, name, address);
+    }
+    getinformation() {
+        return `${this.name} is manager at ${this.address}`;
     }
 }
 // let debashish=new Employee();
@@ -20,3 +55,9 @@ class Employee {
 let emp = new Employee(1, "Akshit", "Mumbai");
 console.log(emp);
 console.log(emp.getinformation());
+console.log("Id-" + emp.empId);
+emp.empId = 100;
+console.log("Id-" + emp.empId);
+let mang1 = new manager(1, "Surya", "Facebook");
+console.log(mang1.getinformation());
+//Getters and Setters
--- TypeScript/classes.ts
+++ TypeScript/classes.ts
@@ -3,13 +3,22 @@ class Employee{
     // name!:string;
     // address!:string; ! dont give errors
 
-    id:number;
-    name:string;
+    #id:number; //#-privvate
+    protected name:string;
     address:string;
+    get empId():number{
+        return this.#id;
+    }
+    set empId(id:number){
+        this.#id=id;
+    }
+static getEmployeeCount():number{
+    return 50;
+}
 
     //constructor(){}
     constructor(id:number,name:string,address:string){
-        this.id=id;
+        this.#id=id;
         this.name=name;
         this.address=address;
     };
@@ -18,7 +27,18 @@ class Employee{
         //return this.name+" "+this.address;
         return `${this.name} stays at ${this.address}`
     }
+    
 
+}
+console.log(Employee.getEmployeeCount());
+class manager extends Employee{
+    constructor(id:number,name:string,address:string){
+        super(id,name,address);
+    }
+    getinformation(): string {
+        return `${this.name} is manager at ${this.address}`
+    }
+   
 }
 // let debashish=new Employee();
 // debashish.id=1;
@@ -27,4 +47,16 @@ class Employee{
 // console.log(debashish);
 let emp=new Employee(1,"Akshit","Mumbai");
 console.log(emp);
-console.log(emp.getinformation());
+console.log(emp.getinformation()); 
+
+console.log("Id-"+emp.empId);//used getter method
+emp.empId=100//used setter method
+console.log("Id-"+emp.empId);//used getter method
+
+let mang1=new manager(1,"Surya","Facebook");
+console.log(mang1.getinformation());
+
+
+
+
+
